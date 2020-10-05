39f607a7f5ac1c5747e4501c8ae02bbf7dd66f23
ActiveZ <activez.git@gmail.com>
2020-10-06 01:03:06 +0200

    pion tourne

--- plateau_graphic.py
+++ plateau_graphic.py
@@ -1,4 +1,6 @@
 import pygame
+from data import cases_data
+
 
 successes, failures = pygame.init()
 print("Initializing pygame: {0} successes and {1} failures.".format(successes, failures))
@@ -23,16 +25,17 @@ class Player(pygame.sprite.Sprite):
         # self.image.fill(WHITE)
         self.image = pygame.image.load("images/pion_chapeau.png")
         self.rect = self.image.get_rect()  # Get rect of some size as 'image'.
-        # self.image = pygame.transform.rotozoom(self.image,0,0.1)
+        self.rect.center = (cases_data[0]["x"], cases_data[0]["y"]) # case départ
 
-        self.velocity = [0, 0]
+        self.velocity = [0, 0] # [x, y]
 
     def update(self):
         self.rect.move_ip(*self.velocity)
 
 
 player = Player()
-running = True
+running = False
+# running = True
 while running:
     dt = clock.tick(FPS) / 1000  # Returns milliseconds between each call to 'tick'. The convert time to seconds.
     # screen.fill(BLACK)  # Fill the screen with background color.
@@ -55,11 +58,43 @@ while running:
             elif event.key == pygame.K_q or event.key == pygame.K_d or event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 player.velocity[0] = 0
 
+    player.velocity[0] = -200 * dt  # X 200 pixels per second
+    running = player.rect.centerx != cases_data[10]["x"] # simple visite
+    # player.velocity[1] = -200 * dt  # Y 200 pixels per second
+
+
     player.update()
+    print("x:", player.rect.centerx, "y:", player.rect.centery) # affiche les coordonnées du centre du pion
 
     screen.blit(img_plateau,(0,0))
     screen.blit(player.image, player.rect)
     pygame.display.update()  # Or pygame.display.flip()
 
+
+running = True
+while running:
+    dt = clock.tick(FPS) / 1000  # Returns milliseconds between each call to 'tick'. The convert time to seconds.
+
+
+    for c in cases_data:
+        if c == 40: break # on saute la case prison
+        for event in pygame.event.get(): running = event.type != pygame.QUIT
+
+        while player.rect.centerx != cases_data[c]["x"] or player.rect.centery != cases_data[c]["y"]:
+            if player.rect.centerx < cases_data[c]["x"]: player.velocity[0] = +1
+            elif player.rect.centerx > cases_data[c]["x"]: player.velocity[0] = -1
+            else: player.velocity[0] = 0
+            if player.rect.centery < cases_data[c]["y"]: player.velocity[1] = +1
+            elif player.rect.centery > cases_data[c]["y"]: player.velocity[1] = -1
+            else: player.velocity[1] = 0
+
+            player.update()
+
+            print("c:", c, "x:", player.rect.centerx, "y:", player.rect.centery) # affiche les coordonnées du centre du pion
+
+            screen.blit(img_plateau,(0,0))
+            screen.blit(player.image, player.rect)
+            pygame.display.update()  # Or pygame.display.flip()
+
 print("Exited the game loop. Game will quit...")
-quit()  # Not actually necessary since the script will exit anyway.
+quit()
